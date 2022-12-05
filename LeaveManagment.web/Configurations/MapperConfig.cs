90c48b4b2bf6bf63852b4f955f750e3e4af23b23
AbdelrahmanMashaly <abdelrahmanmashaly24@outlook.com>
2022-12-05 12:12:07 +0200

    Modified the crud screens and added datatables and swal. End of Leave Type Management Module

---
+++ LeaveManagment.web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagment.web.Data.Migrations;
+using LeaveManagment.web.Models;
+
+namespace LeaveManagment.web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagment.web/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagment.web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name ="Default Number Of days")]
+        [Required]
+        [Range(0,25 ,ErrorMessage = "Please Enter Valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
